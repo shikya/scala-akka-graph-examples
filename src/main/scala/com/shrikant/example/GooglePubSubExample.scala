baa8b8793f1a0642bec5cd996a07b057239887cc
Shrikant Sonone <shrikantsonone@gmail.com>
2018-12-26 15:46:01 +0530

    Added recive message.

--- src/main/scala/com/shrikant/example/GooglePubSubExample.scala
+++ src/main/scala/com/shrikant/example/GooglePubSubExample.scala
@@ -1,15 +1,18 @@
 package com.shrikant.example
 
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.{KeyFactory, PrivateKey}
 import java.util.Base64
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.googlecloud.pubsub.scaladsl.GooglePubSub
-import akka.stream.alpakka.googlecloud.pubsub.{PubSubMessage, PublishRequest}
+import akka.stream.alpakka.googlecloud.pubsub.{AcknowledgeRequest, PubSubMessage, PublishRequest, ReceivedMessage}
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 object GooglePubSubExample extends App {
   implicit val actorSystem = ActorSystem()
@@ -30,4 +33,18 @@ object GooglePubSubExample extends App {
   val publishFlow: Flow[PublishRequest, Seq[String], NotUsed] = GooglePubSub.publish(projectId, apiKey, clientEmail, privateKey, topic)
 
   val publishedMessageIds: Future[Seq[Seq[String]]] = source.via(publishFlow).runWith(Sink.seq)
+
+  // Receive the message
+
+  val subscriptionSource: Source[ReceivedMessage, NotUsed] = GooglePubSub.subscribe(projectId, apiKey, clientEmail, privateKey, subscription)
+
+  val ackSink: Sink[AcknowledgeRequest, Future[Done]] = GooglePubSub.acknowledge(projectId, apiKey, clientEmail, privateKey, subscription)
+
+  subscriptionSource.map { message =>
+    println("Got message from google pub sub > "+ message)
+    message.ackId
+  }.groupedWithin(1000, 1.minute)
+    .map(AcknowledgeRequest.apply)
+    .to(ackSink)
+
 }
